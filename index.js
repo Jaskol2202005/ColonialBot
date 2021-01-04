34b8c3858d16d8d5b95cb52ebd7c550e813d74f2
Jaskol2202005 <jaskol2202005@gmail.com>
2021-01-03 19:26:25 -0700

    whitespaces again

--- index.js
+++ index.js
@@ -13,134 +13,134 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
 const prefix = process.env.prefix;
 
 client.once('ready', () => {
-	console.log('Authentication successful');
+  console.log('Authentication successful');
 });
 
 client.login(process.env.token);
 
 client.on('message', message => {
-	console.log(message.content);
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+  console.log(message.content);
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().toLowerCase().split(/ +/);
-	const commandName = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().toLowerCase().split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(commandName)) return;
+  if (!client.commands.has(commandName)) return;
 
-	const command = client.commands.get(commandName);
+  const command = client.commands.get(commandName);
 
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`
 
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${commandName} ${command.usage}\``;
-		}
-		message.channel.send(reply)
-	}else {
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${commandName} ${command.usage}\``;
+    }
+    message.channel.send(reply)
+  }else {
 
-		try {
-			command.execute(message, args)
-		} catch (error) {
-			console.error(error);
-			message.reply('There was an error trying to execute that command!');
-		}
-	}
+    try {
+      command.execute(message, args)
+    } catch (error) {
+      console.error(error);
+      message.reply('There was an error trying to execute that command!');
+    }
+  }
 })
 
 
 var options = {
-		host: 'elitebgs.app',
-		path: `/api/ebgs/v5/ticks`,
-		headers: {
-				'Accept': 'application/json'
-			}
-		};
+    host: 'elitebgs.app',
+    path: `/api/ebgs/v5/ticks`,
+    headers: {
+        'Accept': 'application/json'
+      }
+    };
 
 var tick = {
-	value: '',
-	letMeKnow() {
-		console.log(`Tick detected`);
-		message.channel.get(`Tick successful`)
-	},
-	get testVar() {
-		return this.value;
-	},
-	set testVar(value) {
-		this.value = value;
-		this.letMeKnow();
-	}
+  value: '',
+  letMeKnow() {
+    console.log(`Tick detected`);
+    message.channel.get(`Tick successful`)
+  },
+  get testVar() {
+    return this.value;
+  },
+  set testVar(value) {
+    this.value = value;
+    this.letMeKnow();
+  }
 }
 
 function logEvery5Minutes(i) {
-	setTimeout(() => {
-		logEvery5Minutes(++i);
-	}, 300000)
+  setTimeout(() => {
+    logEvery5Minutes(++i);
+  }, 300000)
 }
 
 logEvery5Minutes(0)
 
 let i = 0;
 setInterval(() => {
-	console.log(`tick check`, i++);
-	https.get(options, function (res) {
-			var json = '';
+  console.log(`tick check`, i++);
+  https.get(options, function (res) {
+      var json = '';
 
-			res.on('data', function (chunk) {
-					json += chunk;
-				});
+      res.on('data', function (chunk) {
+          json += chunk;
+        });
 
-				res.on('end', function () {
-					if (res.statusCode === 200) {
-							try {
-									var data = JSON.parse(json);
-									console.log(data);
-									let value = data.updated_at
-								} catch (e) {
-									console.log('Error parsing JSON!');
-								}
-							} else {
-								console.log('Status:', res.statusCode);
-							}
-						});
-					}).on('error', function (err) {
-						console.log('Error:', err);
-					});
+        res.on('end', function () {
+          if (res.statusCode === 200) {
+              try {
+                  var data = JSON.parse(json);
+                  console.log(data);
+                  let value = data.updated_at
+                } catch (e) {
+                  console.log('Error parsing JSON!');
+                }
+              } else {
+                console.log('Status:', res.statusCode);
+              }
+            });
+          }).on('error', function (err) {
+            console.log('Error:', err);
+          });
 
 }, 300000)
 
 https.get(options, function (res) {
-		var json = '';
+    var json = '';
 
-		res.on('data', function (chunk) {
-				json += chunk;
-			});
+    res.on('data', function (chunk) {
+        json += chunk;
+      });
 
-			res.on('end', function () {
-				if (res.statusCode === 200) {
-						try {
-								var data = JSON.parse(json);
-								console.log(data);
-								channel.id(568489134880981002).send(`BGS tick successful`)
-							} catch (e) {
-								console.log('Error parsing JSON!');
-							}
-						} else {
-							console.log('Status:', res.statusCode);
-						}
-					});
-				}).on('error', function (err) {
-					console.log('Error:', err);
-				});
+      res.on('end', function () {
+        if (res.statusCode === 200) {
+            try {
+                var data = JSON.parse(json);
+                console.log(data);
+                channel.id(568489134880981002).send(`BGS tick successful`)
+              } catch (e) {
+                console.log('Error parsing JSON!');
+              }
+            } else {
+              console.log('Status:', res.statusCode);
+            }
+          });
+        }).on('error', function (err) {
+          console.log('Error:', err);
+        });
 
 const server = http.createServer((req, res) => {
-	res.writeHead(200);
-	res.end('ok');
+  res.writeHead(200);
+  res.end('ok');
 });
 server.listen(3000);
