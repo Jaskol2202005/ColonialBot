fda9b47ae959c8d458f479c532bcb4bbceaed4e1
Jaskol2202005 <jaskol2202005@gmail.com>
2021-01-03 22:07:30 -0700

    message test undone

--- index.js
+++ index.js
@@ -138,8 +138,6 @@ https.get(options, function(res) {
   console.log('Error:', err);
 });
 
-channel.id(708839430307184756).send(`Tick successful`)
-
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200);
