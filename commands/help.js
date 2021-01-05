34f872383c388755919c888c1ddc2a0d335ed287
Jaskol2202005 <jaskol2202005@gmail.com>
2021-01-05 12:40:24 -0700

    will change back when help list becomes longer

--- commands/help.js
+++ commands/help.js
@@ -15,15 +15,7 @@ module.exports = {
       data.push(commands.map(command => command.name).join(', '));
       data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-      return message.author.send(data, { split: true })
-	     .then(() => {
-		       if (message.channel.type === 'dm') return;
-		       message.reply('I\'ve sent you a DM with all my commands!');
-	     })
-	     .catch(error => {
-		       console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-		       message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-	     });
+      return message.channel.send(data, { split: true })
     }
     const name = args[0].toLowerCase();
     const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
