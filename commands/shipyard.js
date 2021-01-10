ef437b3fbce5e73af5742ceb940e77d6a88bed0b
Jaskol2202005 <jaskol2202005@gmail.com>
2021-01-10 14:34:06 -0700

    added welcome command, plus added a part to shipyard command

--- commands/shipyard.js
+++ commands/shipyard.js
@@ -4,6 +4,6 @@ module.exports = {
   usage: '',
   args: false,
   execute(message, args) {
-    message.channel.send(`Build ships online and test their stats at:\nhttps://coriolis.io\nhttps://edsy.org`);
+    message.channel.send(`Build ships online and test their stats at:\nhttps://coriolis.io\nhttps://edsy.org\n\nBrowse all ships at: https://inara.cz/galaxy-shipyard/`);
   }
 }
---
+++ commands/welcome.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'welcome',
+  description: 'sends welcome message',
+  usage: '<commander name, or @>',
+  args: false,
+  execute(message, args) {
+    let reply = ''
+    for (var i = 0; i < args.length; i++) {
+      let nameUpper = args[i].charAt(0).toUpperCase() + args[i].slice(1)
+      reply += `${nameUpper} `
+    }
+    reply += `, Welcome! Glad to have you! We're a friendly and casual group, please feel free to contribute or start a conversation.  Ask questions, look for wing mates and make friends!\n\nIf you'd like to join our in game squadron, submit an in game application (search TRCG) and I will approve it within a day or two, if not right away. This will earn you a rank promotion inside our Discord server here, opening up further specialized channels, but there's no obligation and you're welcome to remain as a guest with us.\n\nIf you're interested in getting involved with the squadron, check out the pins :pushpin: here in #general-chat to get an idea on our ranks, and what we do!`
+    message.channel.send(reply);
+  }
+}
