fe12d9d0be12ce569e35f767f82a5a97f9e548ad
Jaskol2202005 <jaskol2202005@gmail.com>
2021-01-12 16:03:46 -0700

    these are already in /shipyard

--- commands/coriolis.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'coriolis',
-  description: 'links to coriolis homepage',
-  usage: '',
-  args: false,
-  execute(message, args) {
-    message.channel.send(`https://coriolis.io`);
-  }
-}
--- commands/edsy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'edsy',
-  description: 'links to edsy homepage',
-  usage: '',
-  args: false,
-  execute(message, args) {
-    message.channel.send(`https://edsy.org`);
-  }
-}
