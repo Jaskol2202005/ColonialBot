e59bf4da7036ef128f702559506bc8d0a400e7d4
Jaskol2202005 <jaskol2202005@gmail.com>
2021-01-27 22:35:19 -0700

    another fix

--- commands/welcome.js
+++ commands/welcome.js
@@ -11,6 +11,7 @@ module.exports = {
       nameUpperStr += `${nameUpper} `
     }
     nameUpperStr.slice(0, -1)
+    reply += nameUpperStr
     reply += `, Welcome! Glad to have you! We're a friendly and casual group, please feel free to contribute or start a conversation.  Ask questions, look for wing mates and make friends!\n\nIf you'd like to join our in game squadron, submit an in game application (search TRCG) and I will approve it within a day or two, if not right away. This will earn you a rank promotion inside our Discord server here, opening up further specialized channels, but there's no obligation and you're welcome to remain as a guest with us.\nIf you've joined in-game, you can also join us on inara at: https://inara.cz/squadron/9718/\n\nIf you're interested in getting involved with the squadron, check out the pins :pushpin: here in #general-chat to get an idea on our ranks, and what we do!`
     message.channel.send(reply);
   }
