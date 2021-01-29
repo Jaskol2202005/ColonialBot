e4eb200e929ea50d078b328dc252110302658c21
Jaskol2202005 <jaskol2202005@gmail.com>
2021-01-29 16:10:40 -0700

    added /booster

---
+++ commands/booster.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'booster',
+  description: 'links to exegious video on how to unlock the gfsd booster',
+  usage: '',
+  args: false,
+  execute(message, args) {
+    message.channel.send(`Here's how to unlock the guardian fsd booster: https://youtu.be/J9C9a00-rkQ`)
+  }
+}
