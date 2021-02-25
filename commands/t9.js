61c77678ac50107b8221fcf74f4dbda2918b1ad9
Jaskol2202005 <jaskol2202005@gmail.com>
2021-02-24 18:20:24 -0700

    why...

---
+++ commands/t9.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 't9',
+  description: `dunno why I let them peer pressure me into making this :/`,
+  usage: '',
+  args: false,
+  execute(message, args) {
+    message.channel.send(`https://www.youtube.com/watch?v=uIBPtuwpQQ4`);
+  }
+}
