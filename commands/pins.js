b19ca3f24909d14d706bcc23e6176ec0abbc91ec
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-01 11:06:37 -0700

    added pins command

---
+++ commands/pins.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'pins',
+  description: 'links to pins gif',
+  usage: '',
+  args: false,
+  execute(message, args) {
+    message.channel.send(`https://cdn.discordapp.com/emojis/682556509846110256.gif`);
+  }
+}
