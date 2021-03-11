b2592edd3c0a1fe35a79bf4a3506c08cc2c87b25
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-10 20:17:22 -0700

    w h y

---
+++ commands/flamer.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'flamer',
+  description: 'sends gif',
+  usage: '',
+  args: false,
+  execute(message, args) {
+    message.channel.send(`https://media.tenor.co/videos/8fc6ffea72dc98f67d7d4ea50047461f/mp4`);
+  }
+}
