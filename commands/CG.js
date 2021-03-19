9eb6b60c52210f95972efac09c95e1a5838ca8c7
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-19 16:30:37 -0600

    it begins

---
+++ commands/CG.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+module.exports = {
+  name: 'CG',
+  description: 'Returns details of ongoing or recently finished community goals as are recorded on Inara.',
+  usage: '',
+  args: false,
+  execute(message, args) {
+    let reply = ''
+    var currentTime = new Date();
+    axios.post('https://inara.cz/inapi/v1/', {
+      header: {
+        appName: 'TRCGbot',
+        appVersion: `1.0.0`,
+        isDeveloped: `true`,
+        APIkey: `28r9agl7t5a8gw880cc0wgcowc84k8k0kgsc4k`
+      },
+      events: [
+        {
+          eventName: `getCommunityGoalsRecent`,
+          eventTimestamp: `${currentTime.toISOString()}`,
+          eventData: []
+        }
+      ]
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+}
