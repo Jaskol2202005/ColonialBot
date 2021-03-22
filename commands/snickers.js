873a97f6a3f51f1675ec61ae9fde3b42261495d3
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-22 16:35:54 -0600

    fix

--- commands/snickers.js
+++ commands/snickers.js
@@ -11,7 +11,7 @@ module.exports = {
       let mention = message.mentions.members.first()
       db.get(`snickers${mention}`).then(value => {
         let currentSnickers = value
-        message.channel.reply(`${mention} has ${currentSnickers} Snickers`)
+        message.channel.send(`${mention} has ${currentSnickers} Snickers`)
       })
     } else {
       message.channel.reply(`Please mention the person you would like to check`)
