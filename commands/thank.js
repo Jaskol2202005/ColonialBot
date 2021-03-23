ca94e15d341bf1ed85d02b6292804fde173680e3
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-22 21:55:08 -0600

    thank

---
+++ commands/thank.js
@@ -0,0 +1,25 @@
+const Database = require("@replit/database");
+const db = new Database();
+
+module.exports = {
+  name: 'thank',
+  description: 'thank someone, and give them a snickers',
+  usage: '@<whoever you want to thank>',
+  args: true,
+  execute(message, args) {
+    if (message.mentions.members.size) {
+      let mention = message.mentions.members.first()
+      if (mention.user.id === message.author.id) {
+        message.reply(`You can't thank yourself dummy`)
+      } else {
+        db.get(`snickers${mention}`).then(value => {
+          let currentSnickers = value
+          db.set(`snickers${mention}`, currentSnickers + 1)
+          message.channel.send(`You gave ${mention} a Snickers!\nThey now have ${currentSnickers + 1} Snickers!`)
+        })
+      }
+    } else {
+      message.channel.reply(`Please mention the person you would like to thank`)
+    }
+  }
+}
