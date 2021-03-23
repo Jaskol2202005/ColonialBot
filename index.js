b54ab7dec18bbc7796e5a1a10d672e47d1435275
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-23 11:58:01 -0600

    cooldowns?

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const moment = require('moment-timezone');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -49,6 +50,28 @@ client.on('message', message => {
       }
     });
   });
+
+  const { cooldowns } = client;
+
+  if (!cooldowns.has(command.name)) {
+	   cooldowns.set(command.name, new Discord.Collection());
+   }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    if (timestamps.has(message.author.id)) {
+	    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+	    if (now < expirationTime) {
+		    const timeLeft = (expirationTime - now) / 1000;
+		    return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+	    }
+    }
+  }
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
