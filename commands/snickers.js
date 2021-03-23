6225076165da3b4bad83fd20877bd4947e594302
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-23 10:11:22 -0600

    different method

--- commands/snickers.js
+++ commands/snickers.js
@@ -7,13 +7,13 @@ module.exports = {
   usage: '@<whoever you want to check>',
   args: false,
   execute(message, args) {
-    if (message.mentions.members.first()) {
+    try {
       let mention = message.mentions.members.first()
       db.get(`snickers${mention.user.id}`).then(value => {
         let currentSnickers = value
         message.channel.send(`${mention} has ${currentSnickers} Snickers`)
       })
-    } else {
+    } catch (e) {
       let author = message.author.id
       db.get(`snickers${author}`).then(value => {
         let currentSnickers = value
