56fe7248b136ec320c660f4de77f1efaaa84ce45
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-24 15:44:26 -0600

    please?

--- index.js
+++ index.js
@@ -247,13 +247,13 @@ x.registerListener(function(val) {
   });
 });
 
-  (async () => {
-    let feed = await parser.parseURL('https://community.elitedangerous.com/en/galnet-rss');
-    console.log(feed);
-    let currentDate = moment()
-      let reply = `New Galnet news article!\n\n**${feed.items[0].title}**`
-      client.channels.cache.get(`708839430307184756`).send(reply)
-  });
+(async () => {
+  let feed = await parser.parseURL('https://community.elitedangerous.com/en/galnet-rss');
+  console.log(feed);
+  let currentDate = moment()
+  let reply = `New Galnet news article!\n\n****`
+  client.channels.cache.get(`708839430307184756`).send(reply)
+});
 
 const http = require('http');
 const server = http.createServer((req, res) => {
