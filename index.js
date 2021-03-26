ab9b8811cc44889b38ab1e390ccf3d2e8fa7ed82
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-26 13:01:30 -0600

    debug

--- index.js
+++ index.js
@@ -251,7 +251,6 @@ x.registerListener(function(val) {
     let feed = await parser.parseURL('https://community.elitedangerous.com/en/galnet-rss');
     feed.items.pop(1)
     feed = feed.items
-    console.log(feed);
     let content = []
     for (var i = 0; i < feed.length; i++) {
       content.push(feed[i].contentSnippet)
