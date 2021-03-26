c2d66fc9ec6c96c03a8fd4f16f50946abc809621
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-26 11:57:46 -0600

    change

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ x.registerListener(function(val) {
 setInterval(() => {
   (async () => {
     let feed = await parser.parseURL('https://community.elitedangerous.com/en/galnet-rss');
-    let feed = feed.items.pop(1)
+    let feedPopped = feed.items.pop(1)
     console.log(feed);
     db.set("feeded", feed)
   })();
