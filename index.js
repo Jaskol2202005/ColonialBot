d836bc4d267c38cf877b72f6421d17931ffe294f
Jaskol2202005 <jaskol2202005@gmail.com>
2021-03-29 10:47:57 -0600

    fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const prefix = nconf.get(`prefix`);
 client.once('ready', () => {
   console.log('Authentication successful');
   client.user.setActivity('feds die', { type: "LISTENING" })
-  client.api.applications(client.user.id).commands(`825940355547004929`).post({
+  client.api.applications(client.user.id).commands.post({
     data: {
       name: "authorization",
       description: "Modify the authorization list",
