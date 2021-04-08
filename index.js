840741c2878409677e65de7024c1853288483b27
Jaskol2202005 <jaskol2202005@gmail.com>
2021-04-08 09:29:05 -0600

    should work

--- index.js
+++ index.js
@@ -44,6 +44,28 @@ client.ws.on('INTERACTION_CREATE', async interaction => {
   const args = interaction.data.options
   const command = client.commands.get(commandName);
 
+  const { cooldowns } = client;
+
+  if (!cooldowns.has(command.name)) {
+	   cooldowns.set(command.name, new Discord.Collection());
+   }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 1) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    if (timestamps.has(message.author.id)) {
+	    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+	    if (now < expirationTime) {
+		    const timeLeft = (expirationTime - now) / 1000;
+		    return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+	    }
+    }
+  }
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
   try {
     command.execute(interaction, args, client);
   } catch (error) {
@@ -77,54 +99,6 @@ client.on('message', message => {
       }
     });
   });
-
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  const args = message.content.slice(prefix.length).trim().toLowerCase().split(/ +/);
-  const commandName = args.shift().toLowerCase();
-
-  if (!client.commands.has(commandName)) return;
-
-  const command = client.commands.get(commandName);
-
-  const { cooldowns } = client;
-
-  if (!cooldowns.has(command.name)) {
-	   cooldowns.set(command.name, new Discord.Collection());
-   }
-
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.name);
-  const cooldownAmount = (command.cooldown || 1) * 1000;
-
-  if (timestamps.has(message.author.id)) {
-    if (timestamps.has(message.author.id)) {
-	    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-	    if (now < expirationTime) {
-		    const timeLeft = (expirationTime - now) / 1000;
-		    return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-	    }
-    }
-  }
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-  if (command.args && !args.length) {
-    let reply = `You didn't provide any arguments, ${message.author}!`
-
-    if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix}${commandName} ${command.usage}\``;
-    }
-    message.channel.send(reply)
-  } else {
-
-    try {
-      command.execute(message, args)
-    } catch (error) {
-      console.error(error);
-      message.reply('There was an error trying to execute that command!');
-    }
-  }
 })
 
 client.on('guildMemberAdd', member => {
