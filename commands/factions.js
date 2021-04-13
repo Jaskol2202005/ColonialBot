26d814749bb35467bde76be4907d715b9ac5aed1
Jaskol2202005 <jaskol2202005@gmail.com>
2021-04-13 16:17:10 -0600

    fixed formatting

--- commands/factions.js
+++ commands/factions.js
@@ -20,64 +20,64 @@ module.exports = {
         };
 
     https.get(options, function (res) {
-        var json = '';
+      var json = '';
 
-        res.on('data', function (chunk) {
-            json += chunk;
-          });
+      res.on('data', function (chunk) {
+        json += chunk;
+      });
 
-          res.on('end', function () {
-            if (res.statusCode === 200) {
-                try {
-                    var data = JSON.parse(json);
+      res.on('end', function () {
+        if (res.statusCode === 200) {
+          try {
+            var data = JSON.parse(json);
 
-                    if (json === `{}`) {
-                      client.api.interactions(interaction.id, interaction.token).callback.post({
-                        data: {
-                          type: 4,
-                          data: {
-                            content: `Couldn't find that system (Make sure you spelled it right).`
-                          }
-                        }
-                      })
-                    } else if (data.factions.length === 0) {
-                      client.api.interactions(interaction.id, interaction.token).callback.post({
-                        data: {
-                          type: 4,
-                          data: {
-                            content: `This system doesn't have any factions.`
-                          }
-                        }
-                      })
-                    }
-                    let reply = `**${data.name}** - Current state: ${data.factions[0].state}\n\nFactions:\n`;
-
-                    for (var i = 0; i < data.factions.length; i++) {
-                      if (data.factions[i].influence > 0) {
-                        if (data.factions[i].isPlayer) {
-                          reply += `(Player) `
-                        }
-                        reply += `**${data.factions[i].name}** - ${Math.trunc(data.factions[i].influence*1000)/10}%\n`;
-                      }
-                    }
-
-                    client.api.interactions(interaction.id, interaction.token).callback.post({
-                      data: {
-                        type: 4,
-                        data: {
-                          content: reply
-                        }
-                      }
-                    })
-                  } catch (e) {
-                    console.log('Error parsing JSON!');
+            if (json === `{}`) {
+              client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                  type: 4,
+                  data: {
+                    content: `Couldn't find that system (Make sure you spelled it right).`
                   }
-                } else {
-                  console.log('Status:', res.statusCode);
                 }
-              });
-            }).on('error', function (err) {
-              console.log('Error:', err);
-            });
+              })
+            } else if (data.factions.length === 0) {
+              client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                  type: 4,
+                  data: {
+                    content: `This system doesn't have any factions.`
+                  }
+                }
+              })
+            }
+            let reply = `**${data.name}** - Current state: ${data.factions[0].state}\n\nFactions:\n`;
+
+            for (var i = 0; i < data.factions.length; i++) {
+              if (data.factions[i].influence > 0) {
+                if (data.factions[i].isPlayer) {
+                  reply += `(Player) `
+                }
+                reply += `**${data.factions[i].name}** - ${Math.trunc(data.factions[i].influence*1000)/10}%\n`;
+              }
+            }
+
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+              data: {
+                type: 4,
+                data: {
+                  content: reply
+                }
+              }
+            })
+          } catch (e) {
+            console.log('Error parsing JSON!');
+          }
+        } else {
+          console.log('Status:', res.statusCode);
+        }
+      });
+    }).on('error', function (err) {
+      console.log('Error:', err);
+    });
   },
 };
