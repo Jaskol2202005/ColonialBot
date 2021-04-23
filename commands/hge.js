03c3991b60179d0fbe12dbcd20566f2823c5f513
Jaskol2202005 <jaskol2202005@gmail.com>
2021-04-22 22:15:40 -0600

    formatting fix

--- commands/hge.js
+++ commands/hge.js
@@ -8,7 +8,7 @@ module.exports = {
         data: {
           type: 4,
           data: {
-            content: `The following are a list of where to find high grade emissions that contain desired grade 5 manufactured engineering materials.\nMake sure to use https://inara.cz/nearest/ to locate these, as using just the galaxy map may not show you all of the results that will work.\nMake sure you're looking for systems with a population above 10 million or so for optimal results.\n\nFederal systems: Core Dynamics Composites + Proprietary Composites\nImperial systems: Imperial Shielding\nCivil Unrest system faction states: Improvised Components\nWar or Civil War system faction states:  Military Grade Alloys + Military Supercapacitors\nOutbreak system faction states: Pharmaceutical Isolators\nBoom faction system states: Proto Heat Radiators + Proto Light Alloys + Proto Radiolic Alloys`
+            content: `The following are a list of where to find high grade emissions that contain desired grade 5 manufactured engineering materials.\nMake sure to use https://inara.cz/nearest/ to locate these, as using just the galaxy map may not show you all of the results that will work.\nMake sure you're looking for systems with a population above 10 million or so for optimal results.\n\n**Federal systems:** Core Dynamics Composites + Proprietary Composites\n**Imperial systems:** Imperial Shielding\n**Civil Unrest system faction states:** Improvised Components\n**War or Civil War system faction states:**  Military Grade Alloys + Military Supercapacitors\n**Outbreak system faction states:** Pharmaceutical Isolators\n**Boom faction system states:** Proto Heat Radiators + Proto Light Alloys + Proto Radiolic Alloys`
           }
         }
       })
