de5179b0cd9e9c82336a8c87157499df02a514d6
Jaskol2202005 <jaskol2202005@gmail.com>
2021-05-08 09:38:28 -0600

    fix

--- commands/destroy.js
+++ commands/destroy.js
@@ -24,7 +24,7 @@ module.exports = {
           }
         })
         process.exit(0)
-      } else if (args[0].value.indexOf(`empire` || `imperial`)) {
+      } else if (args[0].value.indexOf(`empire` || `imperial`) > -1) {
         client.api.interactions(interaction.id, interaction.token).callback.post({
           data: {
             type: 4,
