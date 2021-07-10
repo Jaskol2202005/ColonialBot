68bff73d64b01ae7f2c5069513c2e889cbfab4b2
Jan Guzik <jaskol2007@outlook.com>
2021-07-10 17:08:40 -0600

    test

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ let Parser = require('rss-parser'); //rss parser for galnet articles (prototype 
 let parser = new Parser();
 
 db.set("firstTime", true)
+db.get("ignoreList").then(value => {
+  console.log(value)
+})
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); //loads directory for commands
 
