df9468fac2b254ccc5e5f90b84cde54a83073dc6
Jan Guzik <jaskol2007@outlook.com>
2021-07-13 18:45:00 -0600

    test

--- commands/faction.js
+++ commands/faction.js
@@ -90,7 +90,7 @@ module.exports = {
                   if (presence[i].pending_states.length === 0) {
                   } else {
                     reply += `\nPending states: `
-                    for (var k = 0; k < presence[i].pending_states.length; i++) {
+                    for (var j = 0; j < presence[i].pending_states.length; i++) {
                       reply += `${presence[i].pending_states[j].state.charAt(0).toUpperCase() + presence[i].pending_states[j].state.slice(1)},`
                     }
                     reply.slice(0, -1);
@@ -98,7 +98,7 @@ module.exports = {
                   if (presence[i].recovering_states.length === 0) {
                   } else {
                     reply += `\nRecovering states: `
-                    for (var l = 0; l < presence[i].recovering_states.length; i++) {
+                    for (var j = 0; j < presence[i].recovering_states.length; i++) {
                       reply += `${presence[i].recovering_states[j].state.charAt(0).toUpperCase() + presence[i].recovering_states[j].state.slice(1)},`
                     }
                     reply.slice(0, -1);
