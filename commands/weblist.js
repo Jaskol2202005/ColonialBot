cafba63805b450ace35cd755351fbd3f8a01c1a5
Jan Guzik <jaskol2007@outlook.com>
2021-09-27 00:23:16 -0600

    didn't work

--- commands/weblist.js
+++ commands/weblist.js
@@ -7,7 +7,7 @@ module.exports = {
       data: {
         type: 4,
         data: {
-          content: "Use \<[inara](https://inara.cz)\> for finding virtually anything in-game\nUse \<[EDDB](https://eddb.io)\> for accurate and easily accesible ttrading data\nUse \<[EDSM](https://edsm.net)\> for finding systems and what resides in them\nBuild ships virtually and test their stats at \<[Coriolis](https://coriolis.io)\> and \<[EDSY](https://edsy.org)\>\nFind mining hotspots and sell locations as well as other niche data on \<[EDTools](https://edtools.cc/miner)\>\nEfficient neutron plotter and road to riches on \<[Spansh](https://spansh.co.uk)\>\n\<[Cmdr's Toolbox](https://cmdrs-toolbox.com)\>, a useful material finder, fleet carrier calculator, and more\n\<[Need fuel?](https://fuelrats.com)\>"
+          content: "Use [inara](https://inara.cz) for finding virtually anything in-game\nUse [EDDB](https://eddb.io) for accurate and easily accesible ttrading data\nUse [EDSM](https://edsm.net) for finding systems and what resides in them\nBuild ships virtually and test their stats at [Coriolis](https://coriolis.io) and [EDSY](https://edsy.org)\nFind mining hotspots and sell locations as well as other niche data on [EDTools](https://edtools.cc/miner)\nEfficient neutron plotter and road to riches on [Spansh](https://spansh.co.uk)\n[Cmdr's Toolbox](https://cmdrs-toolbox.com), a useful material finder, fleet carrier calculator, and more\n[Need fuel?](https://fuelrats.com)"
         }
       }
     })
