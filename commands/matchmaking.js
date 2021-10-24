159c4ca1bacef68710ab52a6ea78fb138f2cc4e2
Jan Guzik <jaskol2007@outlook.com>
2021-10-23 22:41:31 -0600

    command finished

--- commands/matchmaking.js
+++ commands/matchmaking.js
@@ -89,6 +89,15 @@ module.exports = { //command for adding or removing members from the auth list
               }
             }
           })
+        } else {
+          client.api.interactions(interaction.id, interaction.token).callback.post({
+            data: {
+              type: 4,
+              data: {
+                content: "No on in queue"
+              }
+            }
+          })
         }
       })
     })
