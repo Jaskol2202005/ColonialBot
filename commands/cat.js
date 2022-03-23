114bfd169bb87aa3a8730b73b8e9a995852388d4
Jan Guzik <jaskol2007@outlook.com>
2022-03-22 20:43:30 -0600

    testing /cat

---
+++ commands/cat.js
@@ -0,0 +1,33 @@
+const https = require('https');
+
+module.exports = {
+  name: 'cat',
+  description: 'Sends a cat photo',
+  usage: '',
+  execute(interaction, args, client) {
+    var options = {
+      host: 'thecatapi.com',
+      path: `/api/images/get?format=src&type=png`,
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+
+    https.get(options, function (res) {
+      var json = '';
+
+      res.on('data', function (chunk) {
+        json += chunk;
+      });
+
+      res.on('end', function () {
+        if (res.statusCode === 200) {
+          try {
+            var data = JSON.parse(json);
+            console.log(data);
+          }
+        }
+      })
+    }
+  }
+}
